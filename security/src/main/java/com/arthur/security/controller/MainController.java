58a70125ee9946aaada3218179b018aea4d96c5f
arthurd3 <arthur_camposl@yahoo.com>
2025-09-01 10:37:18 -0300

    study spring security

---
+++ security/src/main/java/com/arthur/security/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.arthur.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MainController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "<h1>Welcome!</h1>";
+    }
+}
---
+++ security/src/test/java/com/arthur/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
